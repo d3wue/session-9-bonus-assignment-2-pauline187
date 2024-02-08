8f0504a4449368907bd516e67d6480f034b51105
pauline187 <152404270+pauline187@users.noreply.github.com>
2024-02-08 14:47:17 +0000

    option1und2funktionieren

--- main.py
+++ main.py
@@ -7,22 +7,47 @@ userAgent = {
 
 url ="https://www.transfermarkt.com/bundesliga/startseite/wettbewerb/L1"
 
-r = requests.get(url, headers=userAgent)
-htmlText = r.text
-htmlDocument = bs4.BeautifulSoup(htmlText, "html.parser")
+print("1: Show available teams")
+print("2: Select team and show high level information on the team")
+print("3: Select team and and show all players of the team")
+print("4: Stop the program")
+choice = int(input())
 
-table = htmlDocument.find('table', {'class': 'items'}).find('tbody')
-teams = table.find_all('tr')
-
-for i in range(len(teams)):
-    team = teams[i]
-    name = team.find('td', {'class': 'hauptlink no-border-links'}).text.strip()
+if choice == 1:
+    r = requests.get(url, headers=userAgent)
+    htmlText = r.text
+    htmlDocument = bs4.BeautifulSoup(htmlText, "html.parser")
+    table = htmlDocument.find('table', {'class': 'items'}).find('tbody')
+    teams = table.find_all('tr')
+    for i in range(len(teams)):
+        team = teams[i]
+        name = team.find('td', {'class': 'hauptlink no-border-links'}).text.strip()
+        print(f"{i}: {name}")
+elif choice == 2:
+    teamChoice = int(input("Give the index of the team you are interested in (note they start with 0)\n"))
+    r = requests.get(url, headers=userAgent)
+    htmlText = r.text
+    htmlDocument = bs4.BeautifulSoup(htmlText, "html.parser")
+    table = htmlDocument.find('table', {'class': 'items'}).find('tbody')
+    teams = table.find_all('tr')
+    team = teams[teamChoice]
     info = team.find_all("td", {"class": "zentriert"})
+    marketValue = team.find_all("td", {"class": "rechts"})
     squad = info[1].text
     age = info[2].text
     foreigners = info[3].text
-    marketValue = team.find_all("td", {"class": "rechts"})
     averageMV = marketValue[0].text
     totalMV = marketValue [1].text
-    print(f"{i}:{name} {squad} {age} {foreigners} {averageMV} {totalMV}")
-    
+    print(f"For the team with index {teamChoice}:\nmembers: {squad} \naverage age: {age} \nNumber of Foreigners: {foreigners} \nAverage value: {averageMV} \nTotal value: {totalMV}")
+
+#for i in range(len(teams)):
+#    team = teams[i]
+#    name = team.find('td', {'class': 'hauptlink no-border-links'}).text.strip()
+#    info = team.find_all("td", {"class": "zentriert"})
+#    squad = info[1].text
+#    age = info[2].text
+#    foreigners = info[3].text
+#    marketValue = team.find_all("td", {"class": "rechts"})
+#    averageMV = marketValue[0].text
+#    totalMV = marketValue [1].text
+#    print(f"{i}:{name} {squad} {age} {foreigners} {averageMV} {totalMV}")
