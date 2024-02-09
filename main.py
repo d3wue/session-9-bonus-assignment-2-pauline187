4db28a3b61106324125553da7b6a6c6f36ff8827
pauline187 <152404270+pauline187@users.noreply.github.com>
2024-02-09 22:41:37 +0000

    besserwirdsnicht

--- main.py
+++ main.py
@@ -12,12 +12,10 @@ htmlDocument = bs4.BeautifulSoup(htmlText, "html.parser")
 table = htmlDocument.find('table', {'class': 'items'}).find('tbody')
 teams = table.find_all('tr')
 
-def listPlayers():
-    userAgent = {
-    'User-Agent':
-    'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
-}
-    url = f"https://www.transfermarkt.com/fc-bayern-munchen/startseite/verein/27/saison_id/2023"
+'''Funktion gibt leider auch market value der Spieler, ich weiß aber nicht wie ich die Suche "rechts hauptlink" 
+ignorieren lassen kann.'''
+
+def player_names(url):
     r = requests.get(url, headers=userAgent)
     htmlText = r.text
     htmlDocument = bs4.BeautifulSoup(htmlText, "html.parser")
@@ -51,23 +49,67 @@ while True:
         totalMV = marketValue [1].text
         print(f"{name}: \nMembers: {squad} \nAverage age: {age} \nNumber of foreigners: {foreigners} \nAverage value: {averageMV} \nTotal value: {totalMV}")
     elif choice == 3:
-        listPlayers()
+        choice2 = int(input("Give the index of the team you are interested in (note they start with 0)\n"))
+        if choice2 == 0:
+            urlChoice = "https://www.transfermarkt.com/fc-bayern-munchen/startseite/verein/27/saison_id/2023"
+            player_names(urlChoice)
+        if choice2 == 1:
+            urlChoice = "https://www.transfermarkt.com/bayer-04-leverkusen/startseite/verein/15/saison_id/2023"
+            player_names(urlChoice)
+        if choice2 == 2:
+            urlChoice = "https://www.transfermarkt.com/rasenballsport-leipzig/startseite/verein/23826/saison_id/2023"
+            player_names(urlChoice)
+        if choice2 == 3:
+            urlChoice = "https://www.transfermarkt.com/borussia-dortmund/startseite/verein/16/saison_id/2023"
+            player_names(urlChoice)
+        if choice2 == 4:
+            urlChoice = "https://www.transfermarkt.com/vfl-wolfsburg/startseite/verein/82/saison_id/2023"
+            player_names(urlChoice)
+        if choice2 == 5:
+            urlChoice = "https://www.transfermarkt.com/vfb-stuttgart/startseite/verein/79/saison_id/2023"
+            player_names(urlChoice)
+        if choice2 == 6:
+            urlChoice = "https://www.transfermarkt.com/eintracht-frankfurt/startseite/verein/24/saison_id/2023"
+            player_names(urlChoice)
+        if choice2 == 7:
+            urlChoice = "https://www.transfermarkt.com/borussia-monchengladbach/startseite/verein/18/saison_id/2023"
+            player_names(urlChoice)
+        if choice2 == 8:
+            urlChoice = "https://www.transfermarkt.com/sc-freiburg/startseite/verein/60/saison_id/2023"
+            player_names(urlChoice)
+        if choice2 == 9:
+            urlChoice = "https://www.transfermarkt.com/1-fc-union-berlin/startseite/verein/89/saison_id/2023"
+            player_names(urlChoice)
+        if choice2 == 10:
+            urlChoice = "https://www.transfermarkt.com/tsg-1899-hoffenheim/startseite/verein/533/saison_id/2023"
+            player_names(urlChoice)
+        if choice2 == 11:
+            urlChoice = "https://www.transfermarkt.com/fc-augsburg/startseite/verein/167/saison_id/2023"
+            player_names(urlChoice)
+        if choice2 == 12:
+            urlChoice = "https://www.transfermarkt.com/1-fsv-mainz-05/startseite/verein/39/saison_id/2023"
+            player_names(urlChoice)
+        if choice2 == 13:
+            urlChoice = "https://www.transfermarkt.com/sv-werder-bremen/startseite/verein/86/saison_id/2023"
+            player_names(urlChoice)
+        if choice2 == 14:
+            urlChoice = "https://www.transfermarkt.com/1-fc-koln/startseite/verein/3/saison_id/2023"
+            player_names(urlChoice)
+        if choice2 == 15:
+            urlChoice = "https://www.transfermarkt.com/vfl-bochum/startseite/verein/80/saison_id/2023"
+            player_names(urlChoice)
+        if choice2 == 16:
+            urlChoice = "https://www.transfermarkt.com/1-fc-heidenheim-1846/startseite/verein/2036/saison_id/2023"
+            player_names(urlChoice)
+        if choice2 == 17:
+            urlChoice = "https://www.transfermarkt.com/sv-darmstadt-98/startseite/verein/105/saison_id/2023"
+            player_names(urlChoice)
     elif choice == 4:
         break
     else:
         print("Invalid Option!")
 
-#for i in range(len(teams)):
-#    team = teams[i]
-#    name = team.find('td', {'class': 'hauptlink no-border-links'}).text.strip()
-#    info = team.find_all("td", {"class": "zentriert"})
-#    squad = info[1].text
-#    age = info[2].text
-#    foreigners = info[3].text
-#    marketValue = team.find_all("td", {"class": "rechts"})
-#    averageMV = marketValue[0].text
-#    totalMV = marketValue [1].text
-#    print(f"{i}:{name} {squad} {age} {foreigners} {averageMV} {totalMV}")
+
 
 
 
